8e5a9992c22af4691b69e8d4452d765ca940c878
Om Gupta <ogupta4242@gmail.com>
2026-08-18 15:58:52 -0700

    chore(server): replace hot reload undertow server with jetty

--- site/src/jvmMain/kotlin/xyz/malefic/guptare/server/Main.kt
+++ site/src/jvmMain/kotlin/xyz/malefic/guptare/server/Main.kt
@@ -1,24 +1,16 @@
 package xyz.malefic.guptare.server
 
 import co.touchlab.kermit.Logger
-import org.http4k.core.HttpHandler
 import org.http4k.core.then
 import org.http4k.filter.ServerFilters
-import org.http4k.hotreload.HotReloadServer
-import org.http4k.hotreload.HotReloadable
-import org.http4k.server.Undertow
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
 import xyz.malefic.guptare.server.util.corsPolicy
 
 fun main() {
     val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
-    val server =
-        HotReloadServer
-            .http<ReloadableHttpApp>(Undertow(port), log = { Logger.d { it } }, error = { Logger.e { it } })
-            .start()
+    val app = ServerFilters.Cors(corsPolicy).then(http)
+    val server = app.asServer(Jetty(port)).start()
 
     Logger.i { "Server started on port ${server.port()}!" }
-}
-
-class ReloadableHttpApp : HotReloadable<HttpHandler> {
-    override fun create() = ServerFilters.Cors(corsPolicy).then(http)
 }
